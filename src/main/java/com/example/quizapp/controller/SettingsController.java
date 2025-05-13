b377fa377ee4310d4a7a7546e3c613551020f1ba
Nicholas <n12017035@qut.edu.au>
2025-05-13 13:28:05 +1000

    Code is working. Ready to be merged to prototype1

--- src/main/java/com/example/quizapp/controller/SettingsController.java
+++ src/main/java/com/example/quizapp/controller/SettingsController.java
@@ -103,8 +103,6 @@ public class SettingsController {
     @FXML
     private void handlePersonalisation() {
         setMessageBox("Personalisation tab clicked", 2);
-        //System.out.println(currentUser.getUserName());
-        //System.out.println(currentUser.getEmail());
     }
 
     @FXML
@@ -203,8 +201,6 @@ public class SettingsController {
             Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
             stage.setScene(scene);
         }
-
-
 
     }
 
