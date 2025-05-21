16504572b235e3b63ae478633632860b26adff9f
Angelica Siv <you@example.com>
2025-05-21 13:17:05 +1000

    Implemented code to provide AI feedback on quiz attempts sorted by topic

--- src/main/java/com/example/quizapp/controller/ProgressReportController.java
+++ src/main/java/com/example/quizapp/controller/ProgressReportController.java
@@ -20,6 +20,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom; // remove later
 
+import static com.example.quizapp.controller.QuestionsController.getQuizAttempt;
+import static com.example.quizapp.model.aiFeedbackGenerator.generateFeedback;
+
 public class ProgressReportController {
     @FXML
     private Label quizTopicLabel;
@@ -39,22 +42,9 @@ public class ProgressReportController {
     private String previousPage;
 
     public void initialize() {
-        commentsArea.setText("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Eget dolor morbi non arcu risus. Quis lectus nulla at volutpat diam
-ut venenatis tellus in. Feugiat in fermentum posuere urna nec tincidunt
-praesent semper. Turpis tincidunt id aliquet risus feugiat in.
-Libero volutpat sed cras ornare. Facilisi morbi tempus iaculis urna.
-Bibendum est ultricies integer quis auctor. Eu augue ut lectus arcu.
-Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.
-Gravida neque convallis a cras. Elit ut aliquam purus sit.
-Suspendisse ultrices gravida dictum fusce ut placerat.
-Integer feugiat scelerisque varius morbi enim nunc.
-Amet justo donec enim diam vulputate ut pharetra.
-Sapien pellentesque habitant morbi tristique.
-Lorem sed risus ultricies tristique nulla aliquet.
-Elementum nibh tellus molestie nunc non blandit massa.""");
+        List<QuizAttempt> quizAttempts = new SQLiteQuizAttemptDAOLive().getQuizAttemptsByTopic(topic);
+        String feedback = generateFeedback(quizAttempts);
+        commentsArea.setText(feedback);
 
         // setLineChartData(generateQuizAttempts(10));
 
