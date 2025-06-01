3b795e5b385f6ecee73dfcd8fc5c958ef433b0c4
Nicholas <n12017035@qut.edu.au>
2025-06-01 23:52:51 +1000

    Final commit? - Fixed package javadoc comments

--- src/main/java/com/example/quizapp/controller/package-info.java
+++ src/main/java/com/example/quizapp/controller/package-info.java
@@ -1,5 +1,5 @@
-package com.example.quizapp.controller;
 /**
  * The controller package contains java classes used for managing user input and updating the model and view based on events.
  *
  */
+package com.example.quizapp.controller;
